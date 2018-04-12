2a28d412539e870e8c7428fd5ae5e05921aac662
Duncan Bourquard <bourquardduncan@gmail.com>
2018-04-12 18:40:05 +0200

    Update test_planete

--- scripts/planetes_test.py
+++ scripts/planetes_test.py
@@ -7,14 +7,17 @@ class PlanetesTest(Component):
 
     def update(self, dt):
         current_pos = self.game_object.transform.get_position()
-        r = p2Vec2(current_pos.x - 300, current_pos.y - 300)
-        magnitude = math.sqrt(math.pow(r.x, 2) + math.pow(r.y, 2))
+        print("current_pos : " + str(current_pos.x) + ";" + str(current_pos.y))
+        r = p2Vec2(300 - current_pos.x, 300 - current_pos.y)
+        magnitude = math.sqrt(r.x**2 + r.y**2)
+        #print("magnitude : " + str(magnitude))
         n = p2Vec2(r.x / magnitude, r.y / magnitude)
-        newVel = p2Vec2(-n.x * 50.0, n.y * 50.0) 
-        newVel = p2Vec2(newVel.x * magnitude * magnitude, newVel.y * magnitude * magnitude)
-        print("NewVel x : " + str(newVel.x) + " ; y:" + str(newVel.y))
+        newVel = p2Vec2(n.x * 500.0, n.y * 500.0) 
+        newVel = p2Vec2(newVel.x / magnitude / magnitude, newVel.y / magnitude / magnitude)
+        print("newVel : " + str(newVel.x) + ";" + str(newVel.y))
+        print("oldVel : " + str(self.body.velocity.x) + ";" + str(self.body.velocity.y))
         if self.body:
-            self.body.velocity = newVel
+            self.body.velocity = p2Vec2(self.body.velocity.x + newVel.x, self.body.velocity.y + newVel.y)
 
     def on_trigger_enter(self, collider):
         pass
